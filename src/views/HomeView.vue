ee1fb151c7e75a816f334e428353380f77b00e79
Salima <akcsalima@gmail.com>
2026-08-19 02:44:22 +0200

    Add more music notes to background - 35 total

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -54,6 +54,26 @@ function firstName() {
       <span class="note note-13" style="animation-delay: 2.2s">♪</span>
       <span class="note note-14" style="animation-delay: 4.2s">♫</span>
       <span class="note note-15" style="animation-delay: 1.8s">♪</span>
+      <span class="note note-16" style="animation-delay: 0.8s">♫</span>
+      <span class="note note-17" style="animation-delay: 3.2s">♪</span>
+      <span class="note note-18" style="animation-delay: 5.2s">♫</span>
+      <span class="note note-19" style="animation-delay: 1.2s">♪</span>
+      <span class="note note-20" style="animation-delay: 4.8s">♫</span>
+      <span class="note note-21" style="animation-delay: 2.8s">♪</span>
+      <span class="note note-22" style="animation-delay: 0.3s">♫</span>
+      <span class="note note-23" style="animation-delay: 3.8s">♪</span>
+      <span class="note note-24" style="animation-delay: 1.6s">♫</span>
+      <span class="note note-25" style="animation-delay: 4.4s">♪</span>
+      <span class="note note-26" style="animation-delay: 2.6s">♫</span>
+      <span class="note note-27" style="animation-delay: 0.6s">♪</span>
+      <span class="note note-28" style="animation-delay: 3.4s">♫</span>
+      <span class="note note-29" style="animation-delay: 5.4s">♪</span>
+      <span class="note note-30" style="animation-delay: 1.4s">♫</span>
+      <span class="note note-31" style="animation-delay: 4.6s">♪</span>
+      <span class="note note-32" style="animation-delay: 2.4s">♫</span>
+      <span class="note note-33" style="animation-delay: 0.2s">♪</span>
+      <span class="note note-34" style="animation-delay: 3.6s">♫</span>
+      <span class="note note-35" style="animation-delay: 5.6s">♪</span>
     </div>
 
     <!-- =================================================================
@@ -143,6 +163,26 @@ function firstName() {
 .note-13 { left: 20%; top: 70%; animation: float-diagonal-1 27s infinite ease-in-out; }
 .note-14 { left: 60%; top: 55%; animation: float-up 29s infinite ease-in; }
 .note-15 { left: 80%; top: 65%; animation: float-diagonal-3 26s infinite ease-in-out; }
+.note-16 { left: 12%; top: 35%; animation: float-up 27s infinite ease-in; }
+.note-17 { left: 28%; top: 50%; animation: float-diagonal-1 30s infinite ease-in-out; }
+.note-18 { left: 38%; top: 8%; animation: float-diagonal-2 25s infinite ease-in-out; }
+.note-19 { left: 52%; top: 40%; animation: float-up 31s infinite ease-in; }
+.note-20 { left: 68%; top: 30%; animation: float-diagonal-3 28s infinite ease-in-out; }
+.note-21 { left: 8%; top: 75%; animation: float-diagonal-1 26s infinite ease-in-out; }
+.note-22 { left: 32%; top: 65%; animation: float-up 29s infinite ease-in; }
+.note-23 { left: 48%; top: 75%; animation: float-diagonal-2 32s infinite ease-in-out; }
+.note-24 { left: 62%; top: 12%; animation: float-up 24s infinite ease-in; }
+.note-25 { left: 78%; top: 55%; animation: float-diagonal-3 27s infinite ease-in-out; }
+.note-26 { left: 18%; top: 42%; animation: float-diagonal-1 30s infinite ease-in-out; }
+.note-27 { left: 42%; top: 18%; animation: float-up 26s infinite ease-in; }
+.note-28 { left: 58%; top: 68%; animation: float-diagonal-2 28s infinite ease-in-out; }
+.note-29 { left: 88%; top: 35%; animation: float-up 31s infinite ease-in; }
+.note-30 { left: 22%; top: 58%; animation: float-diagonal-3 25s infinite ease-in-out; }
+.note-31 { left: 72%; top: 72%; animation: float-diagonal-1 29s infinite ease-in-out; }
+.note-32 { left: 2%; top: 22%; animation: float-up 27s infinite ease-in; }
+.note-33 { left: 52%; top: 5%; animation: float-diagonal-2 30s infinite ease-in-out; }
+.note-34 { left: 92%; top: 48%; animation: float-up 26s infinite ease-in; }
+.note-35 { left: 36%; top: 42%; animation: float-diagonal-3 28s infinite ease-in-out; }
 
 @keyframes float-up {
   0% {
